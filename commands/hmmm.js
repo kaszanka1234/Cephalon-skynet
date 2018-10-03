55dafa6266dbf7487b1d09c4ff826aee63f48788
kaszanka1234 <adriankodzis@op.pl>
2018-10-03 21:35:15 +0200

    added damage type name to icon conversion script

--- commands/hmmm.js
+++ commands/hmmm.js
@@ -1,7 +1,10 @@
 var config = require('../config.json');
+
+var icons = require('../utils/damageToIcon.js');
+
 exports.run = (client, message, args) => {
   if(message.guild === null) return;
 	message.delete(message.id);
   if (message.author.id != config.ownerID) return;
-  console.log(process.env.NODE_ENV);
+  message.channel.send(icons.run(args));
 }
--- utils/damageToIcon.js
+++ utils/damageToIcon.js
@@ -1,4 +1,10 @@
 var config = require('../config.json');
-exports.run = (client, message, args) => {
-	var icons = {impact:"1", slash:"2",puncture:"3"};
+exports.run = (damageType) => {
+	var icons = {impact:"<:aImpact:497121355276222474>", slash:"<:aSlash:497121355070963713>",puncture:"<:aPuncture:497121355255250984>",heat:"<:aHeat:497121354949328908>",electricity:"<:aElectricity:497121354810916865>",toxin:"<:aToxin:497121355259445256>",cold:"<:aCold:497121355729338368>",radiation:"<:aRadiation:497121355280416779>",corrosive:"<:aCorrosive:497121355213307925>",magnetic:"<:aMagnetic:497121355276222464>",gas:"<:aGas:497121355637063701>",viral:"<:aViral:497121355469160458>",blast:"<:aBlast:497121355238473740>"};
+	
+	if(icons[damageType] != undefined){
+		return icons[damageType];
+	}else{
+		return damageType
+	}
 }
